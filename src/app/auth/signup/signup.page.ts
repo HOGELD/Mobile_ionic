5b9b68c0b75417100cc65e867dd8b6d384592a95
sharbel chris <sharbel.cosa@gmail.com>
2020-05-04 15:08:08 +0300

    add custom password validator

--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {CustomValidators} from '../../utils/custom-validators';
 
 @Component({
   selector: 'app-signup',
@@ -17,8 +18,40 @@ export class SignupPage implements OnInit {
   ngOnInit() {
     this.registerCredentialsForm = this.formBuilder.group({
       email: [' ', [Validators.required, Validators.email]],
-      password: [' ', [Validators.required, Validators.minLength(6)]]
-    });
+      password: [' ',
+        Validators.compose(
+            [
+                Validators.required,
+              // check whether the entered password has a number
+              CustomValidators.patternValidator(/\d/, {
+                hasNumber: true
+              }),
+              // check whether the entered password has upper case letter
+              CustomValidators.patternValidator(/[A-Z]/, {
+                hasCapitalCase: true
+              }),
+              // check whether the entered password has a lower case letter
+              CustomValidators.patternValidator(/[a-z]/, {
+              hasSmallCase: true
+              }),
+              // check whether the entered password has a special character
+              CustomValidators.patternValidator(/[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {
+                hasSpecialCharacters: true
+              }),
+                Validators.minLength(8)
+            ])
+      ],
+         confirmPassword: [null, Validators.compose([Validators.required])]
+    }, {
+      // check whether our password and confirm password match
+      validator: CustomValidators.passwordMatchValidator
+        }
+    );
+  }
+
+  submit() {
+    // signup
+    console.log(this.registerCredentialsForm.value);
   }
 
 }
